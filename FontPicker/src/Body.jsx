0b260522cf858cd6111161c0fb1f41d8155231ca
evantwidwell <evanski@gmail.com>
2021-06-07 18:11:56 -0500

    working on making components able to update

--- FontPicker/src/Body.jsx
+++ FontPicker/src/Body.jsx
@@ -82,7 +82,7 @@ export default function BodyComponent() {
             </Select>
           </FormControl>
           <Button size="small" variant="contained" onClick={() => randomize()}>
-            Random Subitle Font?
+            Random Body Font?
           </Button>
         </div>
         <div style={bodyStyle}>
