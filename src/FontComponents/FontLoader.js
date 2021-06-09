1a5d66a9ee0a42b3072b2b4d3e2350498399610a
evantwidwell <evanski@gmail.com>
2021-06-09 18:03:09 -0500

    tidying Font Loader

--- src/FontComponents/FontLoader.js
+++ src/FontComponents/FontLoader.js
@@ -3,17 +3,12 @@ import GoogleFontLoader from "react-google-font-loader";
 import { FontContext } from "./FontContext";
 
 export default function FontLoader(){
-  const { body } = useContext(FontContext);
+  const { body, bodyV, title, titleV, subTitle, subV  } = useContext(FontContext);
   const [bodyFont] = body;
-  const { bodyV } = useContext(FontContext);
   const [bodyVariant] = bodyV;
-  const { title } = useContext(FontContext);
   const [titleFont] = title;
-  const { titleV } = useContext(FontContext);
   const [titleVariant] = titleV;
-  const { subTitle } = useContext(FontContext);
   const [subTitleFont] = subTitle;
-  const { subV } = useContext(FontContext);
   const [subVariant] = subV;
 
   function Loader(){
