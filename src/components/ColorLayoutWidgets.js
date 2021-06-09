d8b878d725f7600184df57cffa4ef2aa38431567
evantwidwell <evanski@gmail.com>
2021-06-08 19:04:21 -0500

    added color widget selectin

--- src/components/ColorLayoutWidgets.js
+++ src/components/ColorLayoutWidgets.js
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import BackgroundColorWidget from './BackgroundColorWidget';
 import SecondaryColorWidget from './SecondaryColorWidget';
 import FontColorWidget from './FontColorWidget';
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 const ColorLayoutWidgets = props => {
+  const [ widget, setWidget ] = useState('background')
+  const classes = useStyles();
+
+  const handleChange = (event) => {
+    setWidget(event.target.value)
+  }
+
+  function ColorLoader(){
+    if(widget === 'background'){
+      return <BackgroundColorWidget handleBackgroundColorChange={props.handleBackgroundColorChange} />
+    } else if (widget === 'secondary'){
+      return <SecondaryColorWidget handleSecondaryColorChange={props.handleSecondaryColorChange} />
+    } else {
+      return <FontColorWidget handleFontColorChange={props.handleFontColorChange} />
+    }
+  }
   return (
     <div>
-        <BackgroundColorWidget handleBackgroundColorChange={props.handleBackgroundColorChange} />
-        <SecondaryColorWidget handleSecondaryColorChange={props.handleSecondaryColorChange} />
-        <FontColorWidget handleFontColorChange={props.handleFontColorChange} />
+      <FormControl className={classes.formControl}>
+            <InputLabel>Which Color?</InputLabel>
+            <Select  onChange={handleChange}>
+              
+                <MenuItem  value={"background"}>background</MenuItem>
+                <MenuItem  value={"secondary"}>secondary</MenuItem>
+                <MenuItem  value={"font"}>font</MenuItem>
+            </Select>
+          </FormControl>
+          <ColorLoader />
     </div>
   )
 }
