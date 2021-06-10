97b4b97a194c63347b860957042598ced87cc94e
evantwidwell <evanski@gmail.com>
2021-06-10 18:35:54 -0500

    adding context to color pickers

--- src/components/BackgroundColorWidget.js
+++ src/components/BackgroundColorWidget.js
@@ -6,12 +6,12 @@ class BackgroundColorWidget extends React.Component {
   static contextType = FontContext
   
 state = {
-  backgroundColor: this.context.background[0],
+  backgroundColor: this.context.background[0]
 };
 
 handleChangeComplete = (color, event) => {
   this.setState({ backgroundColor: color.hex,
-}, function(){})
+})
   this.props.handleBackgroundColorChange(color.hex)
   this.context.background[1](color.hex)
   
--- src/components/SecondaryColorWidget.js
+++ src/components/SecondaryColorWidget.js
@@ -3,15 +3,16 @@ import React from 'react';
 import {FontContext} from '../FontComponents/FontContext';
 
 class SecondaryColorWidget extends React.Component {
-
+  static contextType = FontContext
 state = {
-  backgroundColor: '#fff'
+  backgroundColor: this.context.secondary[0]
 };
 
 handleChangeComplete = (color, event) => {
   this.setState({ backgroundColor: color.hex,
 })
   this.props.handleSecondaryColorChange(color.hex)
+  this.context.secondary[1](color.hex)
 };
 
 render() {
